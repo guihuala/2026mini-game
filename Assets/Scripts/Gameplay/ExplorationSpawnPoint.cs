127b02b59394bcc4c75f11343f25b9af94bd8d0f
小桂花 <143058334+guihuala@users.noreply.github.com>
2026-07-26 00:21:41 +0800

    delete：没用的东西

--- Assets/Scripts/Gameplay/ExplorationSpawnPoint.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public sealed class ExplorationSpawnPoint : MonoBehaviour
-{
-    [SerializeField] private string spawnPointId = "Start";
-    public string Id => spawnPointId;
-}
